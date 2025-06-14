3f87e12abe0cb93837c50f9edebee86b8757cacd
gabrieledis <gabrieledis@hotmail.com>
2025-06-13 23:08:43 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-2xl font-bold text-center">
+        Cifras que confirman que Miguel es Bi
+      </h1>
+    </div>
+  );
+}
